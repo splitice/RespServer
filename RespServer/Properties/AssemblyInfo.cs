fd1fb1b57d0cf11b625b418c87cf3491a23220a7
splitice <mat999@gmail.com>
2016-03-14 19:58:28 +1100

    fix versioning

--- RespServer/Properties/AssemblyInfo.cs
+++ RespServer/Properties/AssemblyInfo.cs
@@ -32,6 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("RespServer.Tests")]
+
+// TRAVIS-CI: START REMOVE
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.*")]
+// TRAVIS-CI: END REMOVE
