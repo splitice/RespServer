37ae30eb4f85417502794bdc1c0b22d5f6158237
splitice <mat999@gmail.com>
2016-03-16 11:58:54 +1100

    add stop method

--- RespServer/RespServerListener.cs
+++ RespServer/RespServerListener.cs
@@ -56,5 +56,11 @@ namespace RespServer
 
             Started = true;
         }
+
+        public void Stop()
+        {
+            _server.Unbind();
+            Started = false;
+        }
     }
 }
